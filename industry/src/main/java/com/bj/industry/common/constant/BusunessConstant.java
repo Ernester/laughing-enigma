650a4c3e04bef9a109c55df10a45c789b31886be
fanwendong <shengshi@7lk.com>
2018-10-23 11:40:34 +0800

    Revert "Revert "play""
    This reverts commit de58dd213f693ef01e66f5964fad91c88c533421.

---
+++ industry/src/main/java/com/bj/industry/common/constant/BusunessConstant.java
@@ -0,0 +1,33 @@
+package com.bj.industry.common.constant;
+
+/**
+ * @author fwd
+ */
+public class BusunessConstant {
+
+    /**
+     * 成功
+     */
+    public static final int SUCCESS = 200;
+
+    /**
+     * Bad Request
+     */
+    public static final int BAD_REQUEST = 400;
+
+    /**
+     * 成功
+     */
+    public static final int FORBIDDEN = 403;
+
+    /**
+     * Not Found
+     */
+    public static final int NOT_FOUND = 404;
+
+    /**
+     * Unknown Internal Error
+     */
+    public static final int  INTERNAL_SERVER_ERROR= 500 ;
+
+}
